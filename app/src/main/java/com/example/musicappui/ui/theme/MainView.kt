aa1afac883a761a39d7cd2a6f894255ce2555e6b
git-sajib <samiul.islam.unisel@gmail.com>
2024-08-19 22:42:04 +0600

    Adding Resource Icons and Prepping our DrawerItem for the Drawer

--- app/src/main/java/com/example/musicappui/ui/theme/MainView.kt
+++ app/src/main/java/com/example/musicappui/ui/theme/MainView.kt
@@ -1,20 +1,28 @@
 package com.example.musicappui.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.musicappui.Screen
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
@@ -43,4 +51,29 @@ fun MainView(
         Text(text = "Text", modifier = Modifier.padding(it))
     }
 
+}
+
+@Composable
+fun DrawerItem(
+    selected: Boolean,
+    item: Screen.DrawerScreen,
+    onDrawerItemClicked:()->Unit
+){
+    val background = if (selected) Color.DarkGray else Color.White
+    Row(
+        Modifier.fillMaxWidth()
+            .padding(horizontal = 8.dp, vertical = 16.dp).background(background)
+            .clickable {
+                onDrawerItemClicked()
+            }
+    ) {
+        Icon(painter = painterResource(id = item.icon),
+            contentDescription = item.dTitle,
+            Modifier.padding(end = 8.dp, top = 4.dp)
+        )
+        Text(
+            text = item.dTitle,
+            style = MaterialTheme.typography.headlineSmall,
+        )
+    }
 }
